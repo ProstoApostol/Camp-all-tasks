f63180b71787ad7d2335292d7faf92f304edffe1
Apostol Ihor <Igornet254@gmail.com>
2024-03-19 22:54:46 +0200

    Add files via upload

---
+++ Task_34.(api)/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from typing import Annotated
+
+app = FastAPI()
+
+
+class Numbers(BaseModel):
+    num1: int
+    num2: int
+
+
+@app.post("/sum/")
+async def sum_numbers(
+    numbers: Annotated[Numbers, Depends()]
+):
+    sum_result = numbers.num1 + numbers.num2
+    return {"Sum": sum_result}
